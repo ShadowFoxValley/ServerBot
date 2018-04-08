13f6de047c80974a28357254b791742d29fd7c7c
BuildTools <unconfigured@null.spigotmc.org>
2018-04-08 18:38:52 +0500

    Added function docs

--- shadowDB.py
+++ shadowDB.py
@@ -12,6 +12,9 @@ class mariadb:
         self.db.close()
 
     def get_settings(self):
+        """
+        Получение настроек из базы данных.
+        """
         with self.db.cursor() as cursor:
             sql="SELECT * FROM settings"
             cursor.execute(sql)
@@ -19,6 +22,9 @@ class mariadb:
         return cursor.fetchone()
 
     def set_prefix(self, new_prefix):
+        """
+        Установка нового префикса для команд.
+        """
         with self.db.cursor() as cursor:
             sql="UPDATE settings SET prefix=%s"
             cursor.execute(sql, (new_prefix,))
@@ -26,6 +32,9 @@ class mariadb:
         return cursor.fetchone()
 
     def get_status(self, user):
+        """
+        Получение статуса пользователя.
+        """
         with self.db.cursor() as cursor:
             sql="SELECT status FROM users WHERE discord_id=%s"
             cursor.execute(sql, (user,))
@@ -33,6 +42,9 @@ class mariadb:
         return cursor.fetchone()[0]
 
     def give_coin(self, user_id):
+        """
+        Выдача коинов пользователю.
+        """
         with self.db.cursor() as cursor:
             sql="UPDATE users SET points=points+1 WHERE discord_id=%s"
             cursor.execute(sql, (user_id,))
@@ -40,6 +52,9 @@ class mariadb:
         return cursor.fetchone()
 
     def  new_user(self, member):
+        """
+        Создание нового пользователя в базе данных.
+        """
         with self.db.cursor() as cursor:
             sql="INSERT INTO users (discord_id, username) VALUES (%s, %s)"
             cursor.execute(sql, (member.id, member.name))
